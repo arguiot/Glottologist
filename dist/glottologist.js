dbc460aad7ae8fe2582ad632d61786725271e3e6
Arthur Guiot <arguiot@gmail.com>
2017-10-31 13:30:10 +0800

    Build

--- dist/glottologist.js
+++ dist/glottologist.js
@@ -4,7 +4,21 @@
 */
 
 class Glottologist {
-  
+  t(phrase, lang="en", source="auto") {
+  	return new Promise((resolve, reject) => {
+  		const url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl="
+  	            + source + "&tl=" + lang + "&dt=t&q=" + encodeURI(phrase);
+  		fetch(url).then(res => {
+  			res.json().then(data => {
+  				resolve(data[0][0][0])
+  			}).catch(e => {
+  				reject(e)
+  			})
+  		}).catch(e => {
+  			reject(e)
+  		})
+  	})
+  }
 }
 // Browserify / Node.js
 if (typeof define === "function" && define.amd) {

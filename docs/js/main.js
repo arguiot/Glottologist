613e2b5f423598b1704c1ef627ee07f839227865
Arthur Guiot <arguiot@gmail.com>
2017-11-05 17:11:45 +0800

    Fixed menu + bottom

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,36 @@
 const $ = new DisplayJS();
 
+
+$.scroll(() => {
+    const distance = $.scrollTop();
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    if (distance > window.innerHeight - 20 && distance + window.innerHeight < docHeight) {
+        $.all(".menu-icon__line", el => {
+            $.css(el, "background", "black")
+        })
+    } else {
+        $.all(".menu-icon__line", el => {
+            $.css(el, "background", "white")
+        })
+    }
+})
+
 $.on(".menu-icon", "click", () => {
-	$.toggleClass("body", "nav-active")
+    $.toggleClass("body", "nav-active")
+	if ($.is("body", ".nav-active")) {
+		$.all(".menu-icon__line", el => {
+            $.css(el, "background", "black")
+        })
+	} else {
+		$.all(".menu-icon__line", el => {
+            $.css(el, "background", "white")
+        })
+	}
 })
 $.all(".black", el => {
-	$.on(el, "click", () => {
-		$.toggleClass("body", "nav-active")
-	})
+    $.on(el, "click", () => {
+        $.toggleClass("body", "nav-active")
+    })
 })

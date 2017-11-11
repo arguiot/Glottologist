94e3e5dd729ae91238554ad2cadf68edab2ff05c
Arthur Guiot <arguiot@gmail.com>
2017-11-11 18:38:41 +0800

    Improved stability

--- src/includes/functions/get.js
+++ src/includes/functions/get.js
@@ -1,10 +1,18 @@
-get(name, lang="auto", obj={}) {
-	let data = {}
-	if(typeof lang == 'object') {
-		data = lang
-		lang = typeof obj == 'string' ? obj : "auto"
+get(name, lang = "auto", obj = {}) {
+    let data = {}
+    if (typeof lang == 'object') {
+        data = lang
+        lang = typeof obj == 'string' ? obj : "auto"
+    }
+	let tmp;
+	let result;
+    if (lang == "auto") {
+		tmp = this.data[name][new String(this.lang).split("-")[0]]
+        result = tmp == "undefined" ? this.data[name][Object.keys(this.data[name])[0]] : tmp
+    } else {
+		tmp = this.data[name][lang];
+		result = tmp == "undefined" ? this.data[name][Object.keys(this.data[name])[0]] : tmp
 	}
-	const result = lang == "auto" ? this.data[name][new String(this.lang).split("-")[0]] : this.data[name][lang]
 
-	return eval("\`"+ result +"\`")
+    return eval("\`" + result + "\`")
 }
